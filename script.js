f888b65e7b5c3ad80dcfbe2f243fb3a720d69c40
Calo <79152594+Objectivitix@users.noreply.github.com>
2022-10-01 13:25:15 -0400

    Use JS to generate SVG markup; correct fonts

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const icons = document.querySelectorAll("div[data-icon]");
+
+icons.forEach(icon => {
+  const svgNS = "http://www.w3.org/2000/svg";
+  const svg = document.createElementNS(svgNS, "svg");
+  svg.setAttribute("xmlns", svgNS);
+  svg.setAttribute("viewBox", "0 0 24 24");
+
+  const use = document.createElementNS(svgNS, "use");
+  use.setAttribute("href", `#${icon.dataset.icon}-icon`);
+  svg.appendChild(use);
+
+  icon.replaceWith(svg);
+});
+
 const menuToggle = document.querySelector(".menu-toggle");
 const menu = document.querySelector("aside");
 
